4ea409498aabf62bb05948d01265041c7ac46c90
Cristian <cristian10.sicoe@gmail.com>
2021-12-21 21:43:19 +0200

    Done

---
+++ app/src/main/java/ro/upt/sma/context/activity/ActivityRecognitionHandler.kt
@@ -0,0 +1,30 @@
+package ro.upt.sma.context.activity
+
+import android.app.PendingIntent
+import android.content.Context
+import com.google.android.gms.location.ActivityRecognition
+import com.google.android.gms.location.ActivityRecognitionClient
+
+class ActivityRecognitionHandler(context: Context) {
+
+    private val client: ActivityRecognitionClient = ActivityRecognition.getClient(context)
+
+    fun registerPendingIntent(): PendingIntent {
+
+        // TODO 5: Create a pending intent for ActivityRecognitionService and register for updates to activity recognition client.
+        val intent = Intent(client.applicationContext, ActivityRecognitionService::class.java).apply {
+
+        }
+
+        val newPendingIntent = PendingIntent.getService(client.applicationContext, 0, intent, 0)
+
+        client.requestActivityUpdates(0,newPendingIntent)
+
+        return newPendingIntent
+    }
+
+    fun unregisterPendingIntent(pendingIntent: PendingIntent) {
+        client.removeActivityUpdates(pendingIntent)
+    }
+
+}
---
+++ app/src/main/java/ro/upt/sma/context/location/LocationHandler.kt
@@ -0,0 +1,31 @@
+package ro.upt.sma.context.location
+
+import android.annotation.SuppressLint
+import android.content.Context
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationServices
+
+@SuppressLint("MissingPermission")
+class LocationHandler(context: Context) {
+
+    private val client: FusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(context)
+
+    fun registerLocationListener(locationCallback: LocationCallback) {
+        // TODO 1: Create a LocationRequest with PRIORITY_HIGH_ACCURACY and smallest displacement to 10m.
+        val locationRequest = LocationRequest.create()
+            .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+            .setSmallestDisplacement(10f)
+        // TODO 2: Register request and callback with the fused location service client.
+        client.requestLocationUpdates(
+            locationRequest,
+            locationCallback,
+            Looper.getMainLooper()
+
+    }
+
+    fun unregisterLocationListener(locationCallback: LocationCallback) {
+        client.removeLocationUpdates(locationCallback)
+    }
+
+}
